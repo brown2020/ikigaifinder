a044ffe6b03dccac1e4da93408727f03ee3c3982
Steve Brown <brown2020@gmail.com>
2024-09-18 13:32:39 -0700

    wording

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
-  title: "Find Your Ikigai",
+  title: "Ikigai Finder",
   description: "Find your Ikigai by answering a series of questions.",
 };
 
